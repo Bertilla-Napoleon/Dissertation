4e13de9104daa5a333979f6c5dbd7e89733be2d7
Bertilla-Napoleon <bertillanapoleon@gmail.com>
2025-08-06 22:48:44 +0100

    Update dissertSIM.py

--- dissertSIM.py
+++ dissertSIM.py
@@ -1,191 +1,109 @@
+import os
+import glob
+from PIL import Image
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-import random
-import numpy as np
+from torchvision import models, transforms
+from torch.utils.data import Dataset, DataLoader
 
-class BasicBlock(nn.Module):
-    expansion = 1
+IMG_SIZE = 224
+BATCH_SIZE = 128
+EPOCHS = 100
+TEMPERATURE = 0.5
+LR = 3e-4
+DATA_DIR = '/db/shared/phenotyping/PlantNet/train'
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
-                               stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
+simclr_transform = transforms.Compose([
+    transforms.RandomResizedCrop(IMG_SIZE, scale=(0.2, 1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+    transforms.RandomGrayscale(p=0.2),
+    transforms.GaussianBlur(kernel_size=23),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
 
-    def forward(self, x):
-        identity = x if self.downsample is None else self.downsample(x)
-        out = self.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += identity
-        out = self.relu(out)
-        return out
-
-class ResNet(nn.Module):
-    def __init__(self, block, layers, out_dim=128):
-        super(ResNet, self).__init__()
-        self.in_channels = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        self.layer1 = self._make_layer(block, 64,  layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.projector = nn.Sequential(
-            nn.Linear(512 * block.expansion, 512),
-            nn.ReLU(),
-            nn.Linear(512, out_dim)
-        )
-
-    def _make_layer(self, block, out_channels, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.in_channels != out_channels * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, out_channels * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels * block.expansion)
-            )
-
-        layers = [block(self.in_channels, out_channels, stride, downsample)]
-        self.in_channels = out_channels * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.in_channels, out_channels))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.relu(self.bn1(self.conv1(x)))
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.projector(x)
-        return F.normalize(x, dim=1)
-
-def ResNet18SimCLR(out_dim=128):
-    return ResNet(BasicBlock, [2, 2, 2, 2], out_dim)
-
-class SimCLRTransform:
-    def __init__(self, image_size):
-        self.transform = transforms.Compose([
-            transforms.RandomResizedCrop(image_size),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-            transforms.RandomGrayscale(p=0.2),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
-        ])
-
-    def __call__(self, x):
-        return self.transform(x), self.transform(x)
-
-class SimCLRDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset, transform):
-        self.dataset = dataset
+class UnlabeledDataset(Dataset):
+    def __init__(self, root_dir, transform):
+        self.files = glob.glob(os.path.join(root_dir, '**/*.jpg'), recursive=True)
         self.transform = transform
 
-    def __getitem__(self, index):
-        x, _ = self.dataset[index]
-        xi, xj = self.transform(x)
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.files[idx]).convert('RGB')
+        xi = self.transform(img)
+        xj = self.transform(img)
         return xi, xj
 
-    def __len__(self):
-        return len(self.dataset)
+class SimCLRModel(nn.Module):
+    def __init__(self, base_model='resnet50', projection_dim=128):
+        super().__init__()
+        resnet = models.__dict__[base_model](pretrained=False)
+        self.encoder = nn.Sequential(*list(resnet.children())[:-1])  # Remove fc layer
+        self.projector = nn.Sequential(
+            nn.Linear(resnet.fc.in_features, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, projection_dim)
+        )
 
-class NTXentLoss(nn.Module):
-    def __init__(self, batch_size, temperature=0.5):
-        super(NTXentLoss, self).__init__()
-        self.batch_size = batch_size
-        self.temperature = temperature
-        self.mask = self._get_correlated_mask().cuda()
+    def forward(self, x):
+        h = self.encoder(x).squeeze()
+        z = self.projector(h)
+        return F.normalize(z, dim=1)
 
-    def _get_correlated_mask(self):
-        N = 2 * self.batch_size
-        mask = torch.ones((N, N), dtype=bool)
-        mask.fill_diagonal_(0)
-        for i in range(self.batch_size):
-            mask[i, i + self.batch_size] = 0
-            mask[i + self.batch_size, i] = 0
-        return mask
+def nt_xent_loss(z1, z2, temperature):
+    N = z1.size(0)
+    z = torch.cat([z1, z2], dim=0)
+    sim = F.cosine_similarity(z.unsqueeze(1), z.unsqueeze(0), dim=2)
+    sim = sim / temperature
 
-    def forward(self, z_i, z_j):
-        N = 2 * self.batch_size
-        z = torch.cat((z_i, z_j), dim=0)
-        sim = F.cosine_similarity(z.unsqueeze(1), z.unsqueeze(0), dim=2)
-        sim = sim / self.temperature
-        positives = torch.cat([torch.diag(sim, self.batch_size),
-                               torch.diag(sim, -self.batch_size)], dim=0)
-        negatives = sim[self.mask].view(N, -1)
-        logits = torch.cat([positives.unsqueeze(1), negatives], dim=1)
-        labels = torch.zeros(N).long().cuda()
-        return F.cross_entropy(logits, labels)
+    labels = torch.cat([torch.arange(N) for _ in range(2)], dim=0)
+    labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float().to(z.device)
+    mask = torch.eye(labels.shape[0], dtype=torch.bool).to(z.device)
+    labels = labels[~mask].view(labels.shape[0], -1)
+    sim = sim[~mask].view(sim.shape[0], -1)
 
-def train_simclr(model, loader, optimizer, criterion, device, epochs):
-    model.train()
-    for epoch in range(epochs):
+    positives = sim[labels.bool()].view(sim.shape[0], -1)
+    negatives = sim[~labels.bool()].view(sim.shape[0], -1)
+
+    logits = torch.cat([positives, negatives], dim=1)
+    labels = torch.zeros(logits.shape[0], dtype=torch.long).to(z.device)
+    return F.cross_entropy(logits, labels)
+
+def train_simclr():
+    dataset = UnlabeledDataset(DATA_DIR, simclr_transform)
+    loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=True)
+
+    model = SimCLRModel().to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+
+    for epoch in range(EPOCHS):
+        model.train()
         total_loss = 0
-        for xi, xj in loader:
-            xi, xj = xi.to(device), xj.to(device)
-            zi = model(xi)
-            zj = model(xj)
-            loss = criterion(zi, zj)
+        for step, (x_i, x_j) in enumerate(loader):
+            x_i = x_i.to(DEVICE)
+            x_j = x_j.to(DEVICE)
 
+            z_i = model(x_i)
+            z_j = model(x_j)
+
+            loss = nt_xent_loss(z_i, z_j, TEMPERATURE)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             total_loss += loss.item()
-        print(f"Epoch [{epoch+1}/{epochs}], Loss: {total_loss / len(loader):.4f}")
 
-def main():
-    seed = 42
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
+        avg_loss = total_loss / len(loader)
+        print(f"Epoch [{epoch+1}/{EPOCHS}] - Loss: {avg_loss:.4f}")
+        torch.cuda.empty_cache()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    batch_size = 128
-    image_size = 224
-    out_dim = 128
-    epochs = 100
-    temperature = 0.5
-    data_dir = '/db/shared/phenotyping/PlantNet'
-    train_dir = f'{data_dir}/train'
-
-    base_dataset = ImageFolder(root=train_dir, train=True, download=True)
-    simclr_transform = SimCLRTransform(image_size=image_size)
-    dataset = SimCLRDataset(base_dataset, simclr_transform)
-
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True,
-                        num_workers=4, drop_last=True)
-
-    model = ResNet18SimCLR(out_dim=out_dim).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=3e-4)
-    criterion = NTXentLoss(batch_size=batch_size, temperature=temperature)
-
-    train_simclr(model, loader, optimizer, criterion, device, epochs)
-
-    torch.save(model.state_dict(), "simclr_resnet18_scratch.pt")
+    torch.save(model.encoder.state_dict(), "simclr_pretrained_encoder.pth")
 
 if __name__ == '__main__':
-    main()
+    train_simclr()
